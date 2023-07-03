a0915080954cdffedfdf465391e1e952cd96bcb2
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-03 12:49:43 +0200

    add hover effects and fix grid layout

--- app/components/Popular.tsx
+++ app/components/Popular.tsx
@@ -42,6 +42,13 @@ const Popular = () => {
           </p>
         </div>{' '}
       </aside>
+
+      <section className='trending-container'>
+        01 Reviving Retro PCs What happens when old PCs are given modern
+        upgrades? 02 Top 10 Laptops of 2022 Our best picks for various needs and
+        budgets. 03 The Growth of Gaming How the pandemic has sparked fresh
+        opportunities.
+      </section>
     </>
   );
 };
