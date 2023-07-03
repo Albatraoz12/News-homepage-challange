013974ab384aaaad7b12dca9ee24d7b100c954ca
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-03 21:18:37 +0200

    remove hite spaces from html

--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -6,7 +6,7 @@ const Trending = () => {
       <div className='trending-card'>
         <img src='/images/image-retro-pcs.jpg' alt='PC Image' />
         <div className='inner-card'>
-          <h4>01 </h4>
+          <h4>01</h4>
           <h5>Reviving Retro PCs</h5>
           <p>What happens when old PCs are given modern upgrades?</p>
         </div>
@@ -14,7 +14,7 @@ const Trending = () => {
       <div className='trending-card'>
         <img src='/images/image-top-laptops.jpg' alt='Keyboard image' />
         <div className='inner-card'>
-          <h4>02 </h4>
+          <h4>02</h4>
           <h5>Top 10 Laptops of 2022 </h5>
           <p>Our best picks for various needs and budgets.</p>
         </div>
@@ -22,7 +22,7 @@ const Trending = () => {
       <div className='trending-card'>
         <img src='/images/image-gaming-growth.jpg' alt='handcontroll' />
         <div className='inner-card'>
-          <h4>03 </h4>
+          <h4>03</h4>
           <h5>The Growth of Gaming</h5>
           <p>How the pandemic has sparked fresh opportunities.</p>
         </div>
