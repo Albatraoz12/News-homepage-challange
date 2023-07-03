3d58a34a5c2cfb9b9f87f61945f366ad78cd262a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-07-03 16:06:28 +0200

    fix layout and style for mobile view

--- app/components/Popular.tsx
+++ app/components/Popular.tsx
@@ -44,10 +44,30 @@ const Popular = () => {
       </aside>
 
       <section className='trending-container'>
-        01 Reviving Retro PCs What happens when old PCs are given modern
-        upgrades? 02 Top 10 Laptops of 2022 Our best picks for various needs and
-        budgets. 03 The Growth of Gaming How the pandemic has sparked fresh
-        opportunities.
+        <div className='trending-card'>
+          <img src='/images/image-retro-pcs.jpg' alt='PC Image' />
+          <div className='inner-card'>
+            <h4>01 </h4>
+            <h5>Reviving Retro PCs</h5>
+            <p>What happens when old PCs are given modern upgrades?</p>
+          </div>
+        </div>
+        <div className='trending-card'>
+          <img src='/images/image-top-laptops.jpg' alt='Keyboard image' />
+          <div className='inner-card'>
+            <h4>02 </h4>
+            <h5>Top 10 Laptops of 2022 </h5>
+            <p>Our best picks for various needs and budgets.</p>
+          </div>
+        </div>
+        <div className='trending-card'>
+          <img src='/images/image-gaming-growth.jpg' alt='handcontroll' />
+          <div className='inner-card'>
+            <h4>03 </h4>
+            <h5>The Growth of Gaming</h5>
+            <p>How the pandemic has sparked fresh opportunities.</p>
+          </div>
+        </div>
       </section>
     </>
   );
